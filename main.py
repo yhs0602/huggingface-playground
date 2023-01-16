715e40727e4b5936d14a7a5a4d944a691f9e813f
KYHSGeekCode <jourhyang123@gmail.com>
2023-01-16 13:04:36 +0900

    :sparkles: Connect pyflowchart

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from datasets import load_dataset
 import json
 
-
+from datasets import load_dataset
+from pyflowchart import Flowchart
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if __name__ == "__main__":
     ds = load_dataset("codeparrot/apps", split="train")
     sample = next(iter(ds))
     # non-empty solutions and input_output features can be parsed from text format this way:
     sample["solutions"] = json.loads(sample["solutions"])
     sample["input_output"] = json.loads(sample["input_output"])
-    print(sample)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # print(sample)
+    solutions = sample["solutions"]
+    for solution in solutions:
+        print("========Solution:==========")
+        fc = Flowchart.from_code(solution)
+        print(fc.flowchart())
