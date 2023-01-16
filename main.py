e98458eef1952b37aa15ddc67c2908e499f7388a
KYHSGeekCode <jourhyang123@gmail.com>
2023-01-16 12:42:09 +0900

    :sparkles: Sample code

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from datasets import load_dataset
+import json
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    ds = load_dataset("codeparrot/apps", split="train")
+    sample = next(iter(ds))
+    # non-empty solutions and input_output features can be parsed from text format this way:
+    sample["solutions"] = json.loads(sample["solutions"])
+    sample["input_output"] = json.loads(sample["input_output"])
+    print(sample)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
