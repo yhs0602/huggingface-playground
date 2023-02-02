e4512842fe98d4eb59808de3aa00913bd887d85d
KYHSGeekCode <jourhyang123@gmail.com>
2023-02-02 11:33:37 +0900

    :sparkles: parse every subroutines

--- foo_to_flowchart.py
+++ foo_to_flowchart.py
@@ -1,97 +1,49 @@
 import _ast
 import ast
-from typing import List
+from typing import Dict
+from functools import partial
 
 from pyflowchart import Flowchart
 from pyflowchart import ast_node
 
 
-def iterate_fields_from_ast(ast_obj: _ast.AST, context: str = "") -> List[_ast.AST]:
-    result = []
-    for (
-        ao
-    ) in (
-        ast_obj.body
-    ):  # raises AttributeError: ast_obj along the field path has no body
+def iterate_fields_from_ast(
+    ast_obj: _ast.AST, context: str = ""
+) -> Dict[str, _ast.AST]:
+    result = {}
+    for ao in ast_obj.body:
         if hasattr(ao, "name"):
-            result.append(context + "." + ao.name)
-            result.extend(iterate_fields_from_ast(ao, context + "." + ao.name))
+            result[context + "." + ao.name] = ao
+            result.update(iterate_fields_from_ast(ao, context + "." + ao.name))
     return result
 
 
-def find_field_from_ast(ast_obj: _ast.AST, field: str) -> _ast.AST:
-    """Find a field from AST.
-
-    This function finds the given `field` in `ast_obj.body`, return the found AST object
-    or an `_ast.AST` object whose body attribute is [].
-    Specially, if field="", returns `ast_obj`.
-
-    A field is the *path* to a `def` code block in code (i.e. a `FunctionDef` object in AST). E.g.
-
-    ```
-    def foo():
-        pass
-
-    class Bar():
-        def fuzz(self):
-            pass
-        def buzz(self, f):
-            def g(self):
-                f(self)
-            return g(self)
-
-    Bar().buzz(foo)
-    ```
-
-    Available path:
-
-    - "" (means the whole ast_obj)
-    - "foo"
-    - "Bar.fuzz"
-    - "Bar.buzz"
-    - "Bar.buzz.g"
-
-    Args:
-        ast_obj: given AST
-        field: path to a `def`
-
-    Returns: an _ast.AST object
-    """
-    if field == "":
-        return ast_obj
-
-    field_list = field.split(".")
-    try:
-        for fd in field_list:
-            for (
-                ao
-            ) in (
-                ast_obj.body
-            ):  # raises AttributeError: ast_obj along the field path has no body
-                if hasattr(ao, "name") and ao.name == fd:
-                    ast_obj = ao
-        assert ast_obj.name == field_list[-1], "field not found"
-    except (AttributeError, AssertionError):
-        ast_obj.body = []
-
-    return ast_obj
-
-
-def my_flowchart(code, field, inner=True, simplify=True, conds_align=False):
-    code_ast = ast.parse(code)
-
-    result = iterate_fields_from_ast(code_ast)
-    print(result)
-    field_ast = find_field_from_ast(code_ast, field)
-
+def field_ast_to_flowchart(
+    field_ast: _ast.AST, inner, simplify, conds_align
+) -> Flowchart:
     assert hasattr(field_ast, "body")
-    assert (
-        field_ast.body
-    ), f"{field}: nothing to parse. Check given code and field please."
+    assert field_ast.body, f"nothing to parse. Check given code and field please."
 
     f = field_ast.body if inner else [field_ast]
     p = ast_node.parse(f, simplify=simplify, conds_align=conds_align)
     return Flowchart(p.head)
+
+
+def my_flowchart(code, inner=True, simplify=True, conds_align=False):
+    code_ast = ast.parse(code)
+
+    field_ast = iterate_fields_from_ast(code_ast)
+    mapped = map(
+        partial(
+            field_ast_to_flowchart,
+            inner=inner,
+            simplify=simplify,
+            conds_align=conds_align,
+        ),
+        field_ast.values(),
+    )
+    for flowchart in mapped:
+        print(flowchart.flowchart())
 
 
 if __name__ == "__main__":
@@ -100,6 +52,4 @@ if __name__ == "__main__":
         code = f.read()
 
     print(code)
-    fc = my_flowchart(code, field="", inner=True)
-    print(fc)
-    print(fc.flowchart())
+    fc = my_flowchart(code, inner=True)
