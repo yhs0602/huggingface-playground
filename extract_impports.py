ddc1f18b64e72a9f7645de85912d0693183d17e2
KYHSGeekCode <jourhyang123@gmail.com>
2023-02-09 09:54:10 +0900

    :bookmark: Finish

--- extract_impports.py
+++ extract_impports.py
@@ -19,6 +19,7 @@ def get_imported_functions_and_calls2(file):
     function_calls = []
     defined_vars = set()
     for node in ast.walk(tree):
+        # print(node, type(node), node.__dict__)
         if isinstance(node, ast.Import):
             for alias in node.names:
                 module = alias.name
@@ -38,35 +39,22 @@ def get_imported_functions_and_calls2(file):
                     imported_functions[refer_name] = ("built-in", module)
                 else:
                     # imported_functions[f"{refer_name}.{name.name}"] = f"{module}"
-                    imported_functions[refer_name] = (module, module)
-
+                    imported_functions[refer_name] = (module, refer_name)
         elif isinstance(node, ast.Call):
-            if isinstance(node.func, ast.Attribute):
-                # print(type(node.func.value))
-                if isinstance(node.func.value, ast.Name):
-                    # print(f"{node.func.value.id} is an attribute")
-                    module = node.func.value.id
-                    if module in imported_functions:
-                        # print("Moudle in imported functions")
-                        func_source = (imported_functions[module], "imported")
-                    else:
-                        # print("Module not in imported functions or attributes")
-                        func_source = (module, "variable")
-                    function_calls.append((f"{module}.{node.func.attr}", func_source))
-                elif isinstance(node.func.value, ast.Call):
-                    # print(f"{node.func.value} is a call")
-                    function_calls.append(
-                        (f"{node.func.value}.{node.func.attr}", "unknown")
-                    )
-            elif isinstance(node.func, ast.Name):
-                # print(f"{node.func.id} is a name")
-                if node.func.id in imported_functions:
-                    func_source = (imported_functions[node.func.id], "imported")
-                elif node.func.id in defined_vars:
-                    func_source = (node.func.id, "local_variable")
+            full_name = ""
+            node = node.func
+            while isinstance(node, ast.Attribute):
+                full_name = node.attr + "." + full_name
+                node = node.value
+            if isinstance(node, ast.Name):
+                module = node.id
+                full_name = full_name.removesuffix(".")
+                # print(f"Module: {module}, full_name: {full_name}")
+                if module in imported_functions:
+                    func_source = (imported_functions[module], "imported")
                 else:
-                    func_source = (node.func.id, "variable-or-builtin")
-                function_calls.append((f"{node.func.id}", func_source))
+                    func_source = (module, "valriable")
+                function_calls.append((full_name, func_source))
         elif isinstance(node, ast.FunctionDef):
             defined_vars.add(node.name)
 
@@ -76,8 +64,8 @@ def get_imported_functions_and_calls2(file):
 def process_one_file(file):
     print("Processing file: ", file)
     functions, calls = get_imported_functions_and_calls2(file)
-    print(functions)
-    print(calls)
+    # print(functions)
+    # print(calls)
     print("=========================================")
     usage_list = []
     for call in calls:
@@ -85,18 +73,23 @@ def process_one_file(file):
             continue
         if call[1][0] == "external":
             first_part, other = call[0].split(".", maxsplit=1)
-            print(f"from {first_part} use {other}")
+            # print(f"from {first_part} use {other}")
             usage_list.append((first_part, other))
         else:
-            print(f"from {call[1][0]} use {call[0]}")
+            # print(f"from {call[1][0]} use {call[0]}")
             usage_list.append((call[1][0], call[0]))
     print(usage_list)
+    for usage in usage_list:
+        if usage[0][0] == "external":
+            print(f"from {usage[0][1]} use {usage[1]}")
+        else:
+            print(f"from {usage[0][0]} use {usage[0][1]}.{usage[1]}")
 
 
 if __name__ == "__main__":
-    # process_one_file("foo_to_flowchart.py")
-    # process_one_file("test_import.py")
-    # process_one_file("foobarbaz.py")
-    # process_one_file("foo.py")
-    # process_one_file("extract_impports.py")
+    process_one_file("foo_to_flowchart.py")
+    process_one_file("test_import.py")
+    process_one_file("foobarbaz.py")
+    process_one_file("foo.py")
+    process_one_file("extract_impports.py")
     process_one_file("panda_import_test.py")
